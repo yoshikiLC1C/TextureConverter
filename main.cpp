486eec3caaae5dfb65da8bc378ddd4eb72fd81ed
itouyoshiki <k021g1467@g.neec.ac.jp>
2023-10-03 15:36:07 +0900

    02_03

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <cstdio>
 #include <cstdlib>
 
-int main() {
+int main(int argc, char* argv[]) {
+
+	for (int i = 0; i < argc; i++) {
+		printf(argv[i]);
+		printf("\n");
+	}
+
 	printf("Hello,World!\n");
 
 	system("pause");
